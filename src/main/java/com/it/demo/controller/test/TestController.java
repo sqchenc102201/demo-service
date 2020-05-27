b3280ccb2c2ea44a977fa1efa2e0c27985966b7a
sqchenc102201 <sqchenc102201@163.com>
2020-05-28 07:22:37 +0800

    init demo

---
+++ src/main/java/com/it/demo/controller/test/TestController.java
@@ -0,0 +1,16 @@
+package com.it.demo.controller.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello test world!";
+    }
+
+
+}
